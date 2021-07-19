ba578caa6629ae8cd88be490372c08bf4355c150
jxtxzzw <jxtxzzw@126.com>
2021-07-19 07:31:16 +0000

    更新LeetCode/198.cpp

--- LeetCode/198.cpp
+++ LeetCode/198.cpp
@@ -1,21 +1,27 @@
-int rob(vector<int>& nums) {
-    const int k = nums.size();
-    int ans = 0;
-    if (k == 0) {
-        ans = 0;
-    } else if (k == 1) {
-        ans = nums[0];
-    } else {
-        int dp[k] = {0};
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        const int k = nums.size();
+        int ans = 0;
+        if (k == 0) {
+            ans = 0;
+        } else if (k == 1) {
+            ans = nums[0];
+        } else {
+            int dp[k];
+            for (int i = 0; i < k; i++) {
+                dp[i] = 0;
+            }
 
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+            dp[0] = nums[0];
+            dp[1] = max(nums[0], nums[1]);
 
-        for (int i = 2; i < k; i++) {
-            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+            for (int i = 2; i < k; i++) {
+                dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+            }
+
+            ans = dp[k - 1];
         }
-
-        ans = dp[k - 1];
+        return ans;
     }
-    return ans;
-}
+};
